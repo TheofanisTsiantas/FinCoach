4460a78968d590cb4b9bc24eee6fb59aca91bec5
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-06-28 18:52:08 +0200

    Split of graphs in class

--- v2000_W_Main_Frame.py
+++ v2000_W_Main_Frame.py
@@ -11,6 +11,7 @@ from PyQt5.QtWidgets import (
 )
 
 from v2001_W_MonthsColumn import W_Months_Column
+from v2002_W_GraphsColumn import W_Graphs_Column
 
 IMPORT_BUTTON_STYLE = """ QPushButton { background-color: #e8d8ac; border-radius: 8px;padding:5px; border: 1px solid grey; }"""
 
@@ -26,19 +27,13 @@ class W_Main_Frame(QFrame):
         main_frame_layout.setContentsMargins(5, 5, 5, 5)
         self.setLayout(main_frame_layout)
 
-#        # ---------- Vertical layouts ----------
+        # --- Months
         self.months_menu = W_Months_Column(self)
         main_frame_layout.addWidget(self.months_menu,1)
 
         # --- Graphs
-        graphs_menu = QWidget()
-        graphs_menu.setStyleSheet("border: 1px solid black;")
-        graphs_VL = QVBoxLayout(graphs_menu)
-        label = QLabel("My graphs")
-        graphs_VL.addWidget(label)
-
-        #main_frame_layout.addWidget(graphs_menu)
-        main_frame_layout.addWidget(graphs_menu,4)
+        self.graphs_menu = W_Graphs_Column(self)
+        main_frame_layout.addWidget(self.graphs_menu,4)
 
         # --- Info
         info_menu = QWidget()
--- v2002_W_GraphsColumn.py
+++ v2002_W_GraphsColumn.py
@@ -10,17 +10,12 @@ from PyQt5.QtWidgets import (
 IMPORT_BUTTON_STYLE = """ QPushButton { background-color: #e8d8ac; border-radius: 8px;padding:5px; border: 1px solid grey; }"""
 
 
-class W_Months_Column(QWidget):
+class W_Graphs_Column(QWidget):
     def __init__(self, parent=None):
         super().__init__(parent)
 
         self.setStyleSheet("border: 1px solid black;")
 
         vertical_layout = QVBoxLayout(self)
-        
-        month_import = QPushButton("Import .csv")
-        month_import.setStyleSheet(IMPORT_BUTTON_STYLE)
-        vertical_layout.addWidget(month_import)
-
-        label = QLabel("January 1999")
+        label = QLabel("My graphs")
         vertical_layout.addWidget(label)
