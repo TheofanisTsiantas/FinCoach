0f4f5fcb55809f2ccf1206dd09d2d871b673c27e
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-06-29 13:10:43 +0200

    Redefinition of the application at higher level

--- Controllers/c1000_Controllers.py
+++ Controllers/c1000_Controllers.py
@@ -8,5 +8,13 @@ from Controllers.c1000_Main_Window import Controller_Main_Window
 
 class Controllers:
     def __init__(self):
+        # Initiate the Qt application
+        app = QApplication(sys.argv) # QApplication must get initialized prior to any QObject use
+        app.setApplicationName("My App")     
+        
+        #
         self.main_window_controller = Controller_Main_Window()
         self.month_controller = Controller_Months()
+        
+        # Ensure correct termination of the Qt application
+        sys.exit(app.exec_())
--- Controllers/c1000_Main_Window.py
+++ Controllers/c1000_Main_Window.py
@@ -1,18 +1,12 @@
 # Imports of libraries
-import sys
-from PyQt5.QtWidgets import QApplication
 
 # Imports of custom modules
 from Views.v1000_Main_Window import View_Main_Window
 
 class Controller_Main_Window:
     def __init__(self):
-        # Create view of the main window
-        app = QApplication(sys.argv) # QApplication must get initialized prior to any QObject use
-        app.setApplicationName("My App")      
-
+        # Create view of the main window 
         self.viewObject = View_Main_Window()
         self.viewObject.show()
         #
         #self.modelObject = Model_Months()
-        sys.exit(app.exec_())
