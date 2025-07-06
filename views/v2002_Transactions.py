c7a2abcbda33b6562fee1249eb5bc17e47fa9778
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-07-06 15:15:04 +0200

    Massive code simplification. Definition of transactions using a grid based system.

--- views/v2002_Transactions.py
+++ views/v2002_Transactions.py
@@ -4,12 +4,14 @@ from PyQt5.QtWidgets import (
     QVBoxLayout,
     QHBoxLayout,
     QLabel,
-    QWidget
+    QWidget,
+    QSizePolicy,
+    QGridLayout
 )
 
 import Views.vh1001_View_Styles as vh 
 
-class View_Transactions(QVBoxLayout):
+class View_Transactions(QGridLayout):
     def __init__(self, transactions:dict ={} , parent=None):
         super().__init__(parent)
         vh.neutralize_layout_style(self)
@@ -19,49 +21,31 @@ class View_Transactions(QVBoxLayout):
             label.setStyleSheet("border: 0px; margin: 5px")
             self.addWidget(label)
         else:
+            rowpos = [0] # To enable pass by reference
             for category, transaction_items in transactions.items():
-                self._create_transactions(category, transaction_items)
-        self.addStretch()
+                self._create_transactions(category, transaction_items, rowpos)
 
-    # For each category it creates a group with th title, the sum and all transactions
-    def _create_transactions(self, category:str, transactions:list):
+    # For each category it a grid group with th title, the sum and all transactions
+    def _create_transactions(self, category:str, transactions:list, rowpos):
+        # If the sum of transactions is zero, omit the category
         sum_of_transactions = round(sum(x[0] for x in transactions),1)
         if sum_of_transactions == 0:
             return
-        # Outer horizontal layout of total group
-        horizontal_frame_group = QWidget(); vh.neutralize_widget_style(horizontal_frame_group)
-        horizontal_layout_group = QHBoxLayout(); vh.neutralize_layout_style(horizontal_layout_group)
-        # Verticals for costs and transactions (inside total group)
-        vertical_frame_costs = QWidget(); vh.neutralize_widget_style(vertical_frame_costs)
-        vertical_frame_transactions = QWidget(); vh.neutralize_widget_style(vertical_frame_transactions)
-        #
-        vertical_layout_costs = QVBoxLayout(); vh.neutralize_layout_style(vertical_layout_costs)
-        vertical_layout_transactions = QVBoxLayout(); vh.neutralize_layout_style(vertical_layout_transactions)
-        #
-        vertical_frame_costs.setLayout(vertical_layout_costs)
-        vertical_frame_transactions.setLayout(vertical_layout_transactions)
-        # Set horizontal group to widget
-        horizontal_frame_group.setLayout(horizontal_layout_group)
-        horizontal_layout_group.addWidget(vertical_frame_costs)
-        horizontal_layout_group.addWidget(vertical_frame_transactions)
-        # Add tiles for the category
-        label_transaction_title = QLabel(category)
-        vertical_layout_costs.addWidget(label_transaction_title)
-        #
-        label_expense_sum = QLabel(str(sum_of_transactions)+" CHF")
-        vertical_layout_transactions.addWidget(label_expense_sum)
-
-        for transaction in transactions:
+        # Add category and transactions
+        self.addWidget(QLabel(category),rowpos[0], 0)
+        self.addWidget(QLabel(str(sum_of_transactions)+" CHF"),rowpos[0], 1)
+        rowpos[0] += 1
+        for i, transaction in enumerate(transactions):
             label_transaction = QLabel(transaction[1])
             label_cost = QLabel(str(transaction[0])+" CHF")
-            vertical_layout_costs.addWidget(label_cost)
-            vertical_layout_transactions.addWidget(label_transaction)
-        #    
-        self.addWidget(horizontal_frame_group)
-        # Space (for upcoming widgets afterwards)
-        dummy_widget = QWidget(); vh.neutralize_widget_style(dummy_widget)
-        dummy_layout = QVBoxLayout(dummy_widget); # Necessary for space
-        self.addWidget(dummy_widget)
+            self.addWidget(label_cost,rowpos[0], 0)
+            self.addWidget(label_transaction,rowpos[0], 1)
+            rowpos[0] += 1
+        self.addWidget(QLabel(),rowpos[0], 0)
+        rowpos[0] += 1
+        return
+
+
 
 
 
