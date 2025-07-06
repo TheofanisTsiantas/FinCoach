a4954f21064d287d075ac1b08971935437617b17
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-07-06 14:21:56 +0200

    Introduction of help class for transactions

---
+++ Helpers/Categories.py
@@ -0,0 +1,11 @@
+# The transaction categories
+TRANSACTION_CATEGORIES = {'Appartment':['FUSION IMMOBILIEN', 'WASHMASTER'], 
+                          'Health':['Amavita','Rotpunkt','Atupri','Oschner'], 
+                          'Investments':[], 
+                          'Super Market':['Coop','Migros','Lidl','Aldi','Topolino'], 
+                          'Restaurant, etc.':['Gastro Technopark','TIBITS','FOOD','TOO GOOD TO GO','GUSTRA','DYNAMO','Bar','Cafe'], 
+                          'Transportation':['SBB'], 
+                          'Phone/Internet':['SALT MOBILE'], 
+                          'Entertainment':['KINO'], 
+                          'Other':[]
+                          }
