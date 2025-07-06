0771212b2c50bcf71dbd3fe1f241850d048e4c46
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-07-06 09:54:44 +0200

    Data cleansing (dropna)

--- Model/m0001_Model_Static_Methods.py
+++ Model/m0001_Model_Static_Methods.py
@@ -1,10 +1,24 @@
 import pandas as pd
 
+TRANSACTION_CATEGORIES = {'Appartment':['FUSION IMMOBILIEN', 'WASHMASTER'], 
+                          'Health':['Amavita','Rotpunkt','Atupri','Oschner'], 
+                          'Investments':[], 
+                          'Super Market':['Coop','Migors','Lidl','Aldi','Topolino'], 
+                          'Restaurant, etc.':['Gastro Technopark','TIBITS','FOOD','TOO GOOD TO GO','GUSTRA','DYNAMO','Bar','Cafe'], 
+                          'Transportation':['SBB'], 
+                          'Phone/Internet':['SALT MOBILE'], 
+                          'Entertainment':['KINO'], 
+                          'Other':[]
+                          }
+
+
 def import_data(path:str):
     # Read the month with the replacement data
     df = pd.read_csv(path, sep=";")
     # Remove unecessary data
     df.drop(columns=['ZKB reference', 'Reference number', 'Credit CHF', 'Balance CHF'], inplace=True)
+    # Remove invalid data
+    df.dropna(inplace=True)
     # Transform date to appropriate format
     df['Date'] = pd.to_datetime(df['Date'], dayfirst=True, errors="coerce")
     df['Date'] = df['Date'].dt.month.astype(str) + "-" + df['Date'].dt.year.astype(str)
