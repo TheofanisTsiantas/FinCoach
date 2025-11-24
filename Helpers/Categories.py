563ffd2ddf5a1dee42000aa2989237567b10ab45
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-11-24 17:39:42 +0100

    Extension of categories

--- Helpers/Categories.py
+++ Helpers/Categories.py
@@ -1,11 +1,12 @@
 # The transaction categories
-TRANSACTION_CATEGORIES = {'Appartment':['FUSION IMMOBILIEN', 'WASHMASTER'], 
+TRANSACTION_CATEGORIES = {'Appartment':['FUSION IMMOBILIEN', 'WASHMASTER', 'Elektrizitätswerk','IKEA', 'SERAFE'], 
                           'Health':['Amavita','Rotpunkt','Atupri','Oschner'], 
                           'Investments':[], 
-                          'Super Market':['Coop','Migros','Lidl','Aldi','Topolino'], 
-                          'Restaurant, etc.':['Gastro Technopark','TIBITS','FOOD','TOO GOOD TO GO','GUSTRA','DYNAMO','Bar','Cafe'], 
+                          'Super Market':['Coop','Migros','Lidl','Aldi','Topolino', 'SPAR'], 
+                          'Restaurant, etc.':['Gastro Technopark','TIBITS','FOOD','TOO GOOD TO GO','GUSTRA','DYNAMO','Bar','Cafe', 'GELATERIA', 'BROS BEANS', 'COCHIN',
+                                              'FLUGHAFEBECK','RESTAURANT', 'KAFI SCHNAPS', 'FRISCHER MAX', 'FRAU GEROLDS', 'HAUS AM FLUSS','PHO VIETNAM'], 
                           'Transportation':['SBB'], 
                           'Phone/Internet':['SALT MOBILE'], 
-                          'Entertainment':['KINO'], 
+                          'Entertainment':['KINO','XENIX FILM', 'COMEDYHAUS','RIFFRAFF','TICKETCORNER', 'XENIXFILMCLUB'], 
                           'Other':[]
                           }
