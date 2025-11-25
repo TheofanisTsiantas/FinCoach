94cb0768f3454ff7f6f574b3c9636689e352a56e
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-11-25 15:44:16 +0100

    Adjustments of controller logic to hold the currently selected month.

--- Controllers/c1001_Main_Frame.py
+++ Controllers/c1001_Main_Frame.py
@@ -11,6 +11,7 @@ class Controller_Main_Frame:
     def __init__(self, View_Main_Frame:View_Main_Frame, model:Model):
         self.view_object = View_Main_Frame
         self.model = model
+        self.selected_month = "" # The controller knows the currently selected month (if any)
 
     def read_months(self, path : str):
         res = self.model.read_file(path)
@@ -35,6 +36,8 @@ class Controller_Main_Frame:
     # Commands model to override its data
     def overwrite_dataframe(self, path : str):
         self.model.overwrite_dataframe(path)
+        # Reset selected month to none
+        self.selected_month = ""
         # Update the view of the months
         self._update_months_view()
         # Update the expense evolution plot
@@ -51,10 +54,11 @@ class Controller_Main_Frame:
 
     # Controller fetches appropriate data of newly selected month to update correspoding view graphs 
     def new_month_selected(self, selected_month=""):
-        # Inform the cotroller that transactions must be updated according to selected "month"
-        self._update_transactions_view(selected_month)
-        # Inform the cotroller that pie view must be updated according to selected "month"
-        self._update_expense_distribution_graph_view(selected_month)
+        self.selected_month = selected_month;
+        # Inform the cotroller that transactions must be updated according to selected month
+        self._update_transactions_view()
+        # Inform the cotroller that pie view must be updated according to selected month
+        self._update_expense_distribution_graph_view()
 
     # private method which calls the view to update months
     def _update_months_view(self):
@@ -67,20 +71,20 @@ class Controller_Main_Frame:
         self._update_transactions_view()
 
     # Update the view of the transactions for a selected month
-    def _update_transactions_view(self, selected_month:str=''):
-        if selected_month=='':
+    def _update_transactions_view(self):
+        if self.selected_month=='':
             self.view_object.update_transactions_view({})
         else:
-            transactions = self.model.get_transactions(selected_month)
+            transactions = self.model.get_transactions(self.selected_month)
             self.view_object.update_transactions_view(transactions)
 
     # Update the pie chart of the expense distribution per month
-    def _update_expense_distribution_graph_view(self, selected_month:str=''):
-        if selected_month=='':
+    def _update_expense_distribution_graph_view(self):
+        if self.selected_month=='':
             self.view_object.update_expense_distribution_graph_view({}, "")
         else:
-            expense_distribution, total_monthly_cost = self.model.get_expense_distribution(selected_month)
-            plot_title = selected_month+"(total: "+str(total_monthly_cost)+" CFH)";
+            expense_distribution, total_monthly_cost = self.model.get_expense_distribution(self.selected_month)
+            plot_title = self.selected_month+"(total: "+str(total_monthly_cost)+" CFH)";
             self.view_object.update_expense_distribution_graph_view(expense_distribution, plot_title)
 
     # Update the scatter chart of the transaction distribution through time
