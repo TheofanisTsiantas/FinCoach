bc7c758fd5ffe62caece370427b47f6526112c5d
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-11-25 19:07:02 +0100

    Addition of investments to categories

--- Helpers/Categories.py
+++ Helpers/Categories.py
@@ -1,7 +1,7 @@
 # The transaction categories
 TRANSACTION_CATEGORIES = {'Appartment':['FUSION IMMOBILIEN', 'WASHMASTER', 'Elektrizitätswerk','IKEA', 'SERAFE'], 
                           'Health':['Amavita','Rotpunkt','Atupri','Oschner'], 
-                          'Investments':[], 
+                          'Investments':['Debit Standing order: Theofanis Tsiantas, Regensbergstrasse 87, 8050'], 
                           'Super Market':['Coop','Migros','Lidl','Aldi','Topolino', 'SPAR'], 
                           'Restaurant, etc.':['Gastro Technopark','TIBITS','FOOD','TOO GOOD TO GO','GUSTRA','DYNAMO','Bar','Cafe', 'GELATERIA', 'BROS BEANS', 'COCHIN',
                                               'FLUGHAFEBECK','RESTAURANT', 'KAFI SCHNAPS', 'FRISCHER MAX', 'FRAU GEROLDS', 'HAUS AM FLUSS','PHO VIETNAM'], 
