9e8db9b99561f12a2254f4618a8333120f9af93d
Theofanis Tsiantas <th.tsiantas@icloud.com>
2025-11-25 13:36:26 +0100

    Preparation for transaction removal button

--- views/v2300_Transactions.py
+++ views/v2300_Transactions.py
@@ -6,6 +6,7 @@ from PyQt5.QtWidgets import (
 )
 
 from PyQt5.QtCore import Qt
+from PyQt5.QtCore import pyqtSignal
 
 import Views.vh1001_View_Styles as vh 
 
@@ -32,25 +33,45 @@ class View_Transactions(QGridLayout):
         # Add category and transactions
         label_title = QLabel(category)
         vh.add_label_left_padding_style(label_title)
-        self.addWidget(label_title,rowpos[0], 0)
+        self.addWidget(label_title,rowpos[0], 0, 1, 2)   # Widget, row, col, row-span, col-span 
         label_sum = QLabel(str(sum_of_transactions)+" CHF")
         vh.add_label_left_padding_style(label_sum)
-        self.addWidget(label_sum,rowpos[0], 1)
+        self.addWidget(label_sum,rowpos[0], 2)
         rowpos[0] += 1
         for i, transaction in enumerate(transactions):
             label_transaction = QLabel(transaction[1])
             vh.add_label_left_padding_style(label_transaction)
             label_cost = QLabel(str(transaction[0])+" CHF")
             vh.add_label_left_padding_style(label_cost)
-            self.addWidget(label_cost,rowpos[0], 0)
-            self.addWidget(label_transaction,rowpos[0], 1)
+            self.addWidget(label_cost,rowpos[0], 1)
+            self.addWidget(label_transaction,rowpos[0], 2)
+            self.addWidget(ClickableLabel(" - "),rowpos[0], 0)
             rowpos[0] += 1
         self.addWidget(QLabel(),rowpos[0], 0)
         rowpos[0] += 1
         return
 
 
+class ClickableLabel(QLabel):
 
+    clicked = pyqtSignal()
+
+    def __init__(self, name:str ="" , parent=None):
+        super().__init__(parent)
+        self.setEnabled(True)
+        self.setText(name)
+        self.setStyleSheet("background-color: rgb(250, 50, 50); border: 0px;  color: white; margin: 5px; border-radius:5px; ")
+        self.setAlignment(Qt.AlignCenter) # Align text horizontally and vertically
+        self.clicked.connect(self._label_clicked)
+    
+    def _label_clicked(self):
+        self.setStyleSheet("background-color: rgb(47, 117, 250); color: white; border: 0px; border-radius:5px; margin: 5px")
+
+    def mousePressEvent(self, event):
+        # Remove transaction
+        self.clicked.emit()
+        # Default functionality fallback
+        super().mousePressEvent(event)
 
 
 
